a53306ac1380f8743bdbd91639c2308b52c01973
WINDOWS-11\Administrator <dinhvanhd99@gmail.com>
2024-11-11 22:34:23 +0700

    code email

---
+++ Repositories/Email.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObject.BaseModel;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    internal class Email
+    {
+        private readonly IConfiguration _configuration;
+        public Email(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<bool> SendEmailAsync(EmailModel emailModel)
+        {
+            try
+            {
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress(_configuration["SmtpSettings:SenderName"], _configuration["SmtpSettings:SenderEmail"]));
+
+                if (emailModel.To != null)
+                {
+                    foreach (var email in emailModel.To)
+                    {
+                        emailMessage.To.Add(new MailboxAddress("", email));
+                    }
+                }              
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
