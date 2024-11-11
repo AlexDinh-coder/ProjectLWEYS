e3bab24b520c03c8ddaa00fde7093c3994884daf
WINDOWS-11\Administrator <dinhvanhd99@gmail.com>
2024-11-11 21:06:25 +0700

    modifile payment

--- Repositories/Payment.cs
+++ Repositories/Payment.cs
@@ -20,46 +20,46 @@ namespace Repositories
 
         private static string returnUrl = $"{Environment.GetEnvironmentVariable("WEBPAGE_URL")}/Home/PaymentStatus";
         //var env = Environment.
-        public static QuickPayResquest MoMoPayment(QuickPayResquest request)
-        {
-            var myuuid = Guid.NewGuid();
-            string myuuidAsString = myuuid.ToString();
+        //public static QuickPayResquest MoMoPayment(QuickPayResquest request)
+        //{
+        //    var myuuid = Guid.NewGuid();
+        //    string myuuidAsString = myuuid.ToString();
 
-            string accessKey = "F8BBA842ECF85";
-            string secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
+        //    string accessKey = "F8BBA842ECF85";
+        //    string secretKey = "K951B6PE1waDMi640xX08PD3vg6EkVlz";
 
-            //var request = new QuickPayResquest();
-            request.orderInfo = "pay with MoMo";
-            request.partnerCode = "MOMO";
-            request.redirectUrl = returnUrl;
-            request.ipnUrl = returnUrl;
-            //request.amount = 5000;
-            request.orderId = myuuidAsString;
-            request.requestId = myuuidAsString;
-            request.requestType = "payWithMethod";
-            //request.extraData = "";
-            request.partnerName = "MoMo Payment";
-            request.storeId = "LWEYS Management";
-            request.orderGroupId = "";
-            request.autoCapture = true;
-            request.lang = "vi";
+        //    //var request = new QuickPayResquest();
+        //    request.orderInfo = "pay with MoMo";
+        //    request.partnerCode = "MOMO";
+        //    request.redirectUrl = returnUrl;
+        //    request.ipnUrl = returnUrl;
+        //    //request.amount = 5000;
+        //    request.orderId = myuuidAsString;
+        //    request.requestId = myuuidAsString;
+        //    request.requestType = "payWithMethod";
+        //    //request.extraData = "";
+        //    request.partnerName = "MoMo Payment";
+        //    request.storeId = "LWEYS Management";
+        //    request.orderGroupId = "";
+        //    request.autoCapture = true;
+        //    request.lang = "vi";
 
-            var rawSignature = "accessKey=" + accessKey + "&amount=" + request.amount + "&extraData=" + request.extraData + "&ipnUrl=" + request.ipnUrl + "&orderId=" + request.orderId + "&orderInfo=" + request.orderInfo + "&partnerCode=" + request.partnerCode + "&redirectUrl=" + request.redirectUrl + "&requestId=" + request.requestId + "&requestType=" + request.requestType;
+        //    var rawSignature = "accessKey=" + accessKey + "&amount=" + request.amount + "&extraData=" + request.extraData + "&ipnUrl=" + request.ipnUrl + "&orderId=" + request.orderId + "&orderInfo=" + request.orderInfo + "&partnerCode=" + request.partnerCode + "&redirectUrl=" + request.redirectUrl + "&requestId=" + request.requestId + "&requestType=" + request.requestType;
 
-            request.signature = getSignature(rawSignature, secretKey);
-            var httpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
-            var res = client.PostAsync("https://test-payment.momo.vn/v2/gateway/api/create", httpContent).Result;
-            if (res.IsSuccessStatusCode)
-            {
-                var responseString = res.Content.ReadAsStringAsync().Result;
-                var result = JsonConvert.DeserializeObject<QuickPayResquest>(responseString);
-                return result;
-            }
-            else
-            {
-                return new QuickPayResquest();
-            }
-        }
+        //    request.signature = getSignature(rawSignature, secretKey);
+        //    var httpContent = new StringContent(System.Text.Json.JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+        //    var res = client.PostAsync("https://test-payment.momo.vn/v2/gateway/api/create", httpContent).Result;
+        //    if (res.IsSuccessStatusCode)
+        //    {
+        //        var responseString = res.Content.ReadAsStringAsync().Result;
+        //        var result = JsonConvert.DeserializeObject<QuickPayResquest>(responseString);
+        //        return result;
+        //    }
+        //    else
+        //    {
+        //        return new QuickPayResquest();
+        //    }
+        //}
 
         private static string getSignature(string text, string key)
         {
