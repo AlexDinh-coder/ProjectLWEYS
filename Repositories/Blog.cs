5463a94bc2ff248bc5e65f114d44736f0d923b02
WINDOWS-11\Administrator <dinhvanhd99@gmail.com>
2024-11-11 23:44:35 +0700

    add blog

---
+++ Repositories/Blog.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    internal class Blog
+    {
+        private readonly IConfiguration _configuration;
+        public Blog(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
