2b6db1a1183afd6965a9f5ca387086ca7f165de0
WINDOWS-11\Administrator <dinhvanhd99@gmail.com>
2024-11-11 23:47:43 +0700

    add TotalAccount

---
+++ Repositories/TotalAccount.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    internal class TotalAccount
+    {
+        private readonly IConfiguration _configuration;
+        public TotalAccount(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
