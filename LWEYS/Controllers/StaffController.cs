20f3fbcb920e8710aec86016264b95769abd3a0a
WINDOWS-11\Administrator <dinhvanhd99@gmail.com>
2024-11-11 22:36:21 +0700

    code staff

---
+++ LWEYS/Controllers/StaffController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessObject;
+using LWEYS.Services.Order;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LWEYS.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public async Task<IActionResult> ListService()
+        {
+            var res = await _orderService.GetAllService();
+            ViewBag.ListService = res.DataList;
+            return View();
+        }
+
+        [Authorize(Roles = Role.Admin)]
+        public async Task<IActionResult> GetService(int id)
+        {
+            var res = await _orderService.GetService(id);
+            return Json(res.Data);
+        }
+    }
+}
